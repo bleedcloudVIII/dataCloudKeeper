949e951d007aa5d6bd521771db24251ae753bdbd
bleedcloudVIII <yurikaras8@gmail.com>
2025-02-26 19:32:43 +1000

    methods

--- src/cloudService.ts
+++ src/cloudService.ts
@@ -3,14 +3,26 @@ import type { DownloadRequest } from './proto/interface/downloadRequest';
 import type { DownloadResponse } from './proto/interface/downloadResponse';
 import type { SaveRequest } from './proto/interface/saveRequest';
 import type { SaveResponse } from './proto/interface/saveResponse';
+import { readFile, writeFile } from "fs/promises";
+import * as path from 'path';
 
-export function download(call: grpc.ServerUnaryCall<DownloadRequest, DownloadResponse>, callback: grpc.sendUnaryData<DownloadResponse>) {
-    const l = call.request.location;
-    const fn = call.request.file_name;
-    
-    callback(null, { bytes: `Hello, downloading  ${l}/${fn}\n..........`});
+// TODO обработку ошибок, чтобы не крашилось
+
+export async function download(call: grpc.ServerUnaryCall<DownloadRequest, DownloadResponse>, callback: grpc.sendUnaryData<DownloadResponse>) {
+    const file_name = call.request.file_name;
+    const location = call.request.location;
+    const filePath = path.resolve(__dirname, `./../files/${file_name}`);
+
+    const content = await readFile(filePath)
+    callback(null, { bytes: `${content}` });
 }
 
-export function save(call: grpc.ServerUnaryCall<SaveRequest, SaveResponse>, callback: grpc.sendUnaryData<SaveResponse>) {
+export async function save(call: grpc.ServerUnaryCall<SaveRequest, SaveResponse>, callback: grpc.sendUnaryData<SaveResponse>) {
+    const file_name = call.request.file_name;
+    const bytes = call.request.bytes;
+    const filePath = path.resolve(__dirname, `./../files/${file_name}`);
 
+    await writeFile(filePath, bytes);
+    
+    callback(null, {message: "OK", status: 0});
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as grpc from '@grpc/grpc-js';
 import * as protoLoader from '@grpc/proto-loader';
-import { download } from './cloudService';
+import { download, save } from './cloudService';
 
 const PROTO_PATH = __dirname + '/proto/cloud.proto';
 const PORT = 5000;
@@ -20,6 +20,7 @@ function main() {
     const server = new grpc.Server();
     server.addService((CloudProto.FileService as any).service, {
         download,
+        save
     });
     server.bindAsync(`${HOST}:${PORT}`, grpc.ServerCredentials.createInsecure(), () => {
         console.log(`Server running on http://${HOST}:${PORT}`);
