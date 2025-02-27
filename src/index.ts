b669b5833cdf8a8cf3e5feb4b1fbd1393cabb425
bleedcloudVIII <yurikaras8@gmail.com>
2025-02-28 09:15:27 +1000

    Настройки + тест передаваемых данных

--- src/index.ts
+++ src/index.ts
@@ -6,18 +6,26 @@ const PROTO_PATH = __dirname + '/proto/cloud.proto';
 const PORT = 5000;
 const HOST = "0.0.0.0"
 
-const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+const options: protoLoader.Options = {
     keepCase: true,
     longs: String,
     enums: String,
     defaults: true,
-    oneofs: true
-});
+    oneofs: true,
+    
+}
+
+const serverOptions: grpc.ServerOptions = {
+    "grpc.max_receive_message_length": 1024 * 1024 * 100,
+    "grpc.max_send_message_length": 1024 * 1024 * 100,
+}
+
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, options);
 
 const CloudProto = grpc.loadPackageDefinition(packageDefinition).cloud;
 
 function main() {
-    const server = new grpc.Server();
+    const server = new grpc.Server(serverOptions);
     server.addService((CloudProto.FileService as any).service, {
         download,
         save
